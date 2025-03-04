f82cceb4d400dada5524f6d27923b604126de4a6
timursaadak <tsaadak@gmail.com>
2025-03-05 01:41:15 +0600

    Add homepage (page.tsx)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-"use client";
+// app/page.tsx
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold">Welcome to MeritLand!</h1>
-    </main>
-  );
-}
+    return (
+      <div>
+        <h1>Welcome to Meritland Web!</h1>
+      </div>
+    );
+  }
+  
